858e53265bac2c8b8dc7f939f7add6a59a8db73e
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-14 11:46:22 +0300

    reorder sounds

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState, useEffect, createRef, useRef } from "react";
 import "./App.css";
 import Pad from "./components/Pad";
-import audio from "./audio_loops/120_future_funk_beats_25.mp3";
-import audio2 from "./audio_loops/120_stutter_breakbeats_16.mp3";
-import audio3 from "./audio_loops/Bass Warwick heavy funk groove on E 120 BPM.mp3";
-import audio4 from "./audio_loops/electric guitar coutry slide 120bpm - B.mp3";
-import audio5 from "./audio_loops/FUD_120_StompySlosh.mp3";
-import audio6 from "./audio_loops/GrooveB_120bpm_Tanggu.mp3";
-import audio7 from "./audio_loops/MazePolitics_120_Perc.mp3";
-import audio8 from "./audio_loops/PAS3GROOVE1.03B.mp3";
-import audio9 from "./audio_loops/SilentStar_120_Em_OrganSynth.mp3";
+import audio from "./audio_loops/120_future_funk_beats_25.mp3"; //GiVintageRobot
+import audio2 from "./audio_loops/120_stutter_breakbeats_16.mp3"; //GiEchoRipples
+import audio3 from "./audio_loops/Bass Warwick heavy funk groove on E 120 BPM.mp3"; //GiGuitarBassHead
+import audio4 from "./audio_loops/FUD_120_StompySlosh.mp3"; //GiDrum
+import audio5 from "./audio_loops/electric guitar coutry slide 120bpm - B.mp3"; //GiGuitar
+import audio6 from "./audio_loops/GrooveB_120bpm_Tanggu.mp3"; //FaDrumSteelpan
+import audio7 from "./audio_loops/MazePolitics_120_Perc.mp3"; //GiMaterialsScience
+import audio8 from "./audio_loops/PAS3GROOVE1.03B.mp3"; // FaDrum
+import audio9 from "./audio_loops/SilentStar_120_Em_OrganSynth.mp3"; //GiDominoMask
 import ControlPanel from "./components/ControlPanel";
 
 function App() {
